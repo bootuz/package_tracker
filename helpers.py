0280ac10d684f3dc0a5a78f62bc5525baba106f8
Nart Kenobi <bootuz07@gmail.com>
2019-09-03 13:45:43 +0300

    Complex name

--- helpers.py
+++ helpers.py
@@ -2,14 +2,20 @@
 def parse(history):
     data = {"sender": history[0].UserParameters.Sndr, "recipient": history[0].UserParameters.Rcpn,
             "from": history[0].AddressParameters.CountryFrom.NameRU,
-            "to": history[0].AddressParameters.MailDirect.NameRU, "item": history[0].ItemParameters.ComplexItemName}
+            "to": history[0].AddressParameters.MailDirect.NameRU}
+    try:
+        data["item"] = history[0].ItemParameters.ComplexItemName
+    except AttributeError:
+        data["item"] = "No information"
 
     res = []
     for i in range(len(history)):
         try:
-            res.append(f'{history[i].AddressParameters.OperationAddress.Description} {history[i].OperationParameters.OperAttr.Name}')
+            res.append(f'{history[i].AddressParameters.OperationAddress.Description} '
+                       f'{history[i].OperationParameters.OperAttr.Name}')
         except AttributeError:
-            res.append(f'{history[i].AddressParameters.OperationAddress.Description} {history[i].OperationParameters.OperType.Name}')
+            res.append(f'{history[i].AddressParameters.OperationAddress.Description} '
+                       f'{history[i].OperationParameters.OperType.Name}')
 
     data["points"] = '\n'.join(res)
     return data
